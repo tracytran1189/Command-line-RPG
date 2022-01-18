a63b09a3b98144d20718ac671e7eac7686d16273
Tracy Tran <tracytran1189@gmail.com>
2022-01-18 14:26:15 -0600

    write getStats and getInventory method to pass test

--- ___tests__/Player.test.js
+++ ___tests__/Player.test.js
@@ -6,7 +6,6 @@ jest.mock('../lib/Potion.js');
 console.log(new Potion());
 
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -19,3 +18,23 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
